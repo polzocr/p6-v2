90f58710dc712b3fd0611498582a88c08aa89ac8
Polzocr <micro.cpt.62@outlook.fr>
2022-03-22 10:20:59 +0100

    configuration mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+dotenv.config();
+const NAME = process.env.USER;
+const MDP = process.env.SECRET_PASS;
+const BDD = process.env.CLUSTER_NAME;
+
 const app = express();
 
+mongoose.connect("mongodb+srv://"+ NAME +":" + MDP +"@"+ BDD + ".j5cho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à nodemonMongoDB échouée !'));
+
+
+
+
+  
 module.exports = app;
 
 
