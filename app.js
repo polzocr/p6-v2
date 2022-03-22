a7dbc5f272dcab67aaf717cfabcabb3e1e920200
Polzocr <micro.cpt.62@outlook.fr>
2022-03-22 12:25:45 +0100

    ajout des deux routes get

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
