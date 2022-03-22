3d5df2083fbc4ec390507d8ef5fc1230c8baaf2f
Polzocr <micro.cpt.62@outlook.fr>
2022-03-22 10:21:49 +0100

    configuration CORS et express.json pour récupérer les cors de requetes POST

--- app.js
+++ app.js
@@ -15,10 +15,20 @@ mongoose.connect("mongodb+srv://"+ NAME +":" + MDP +"@"+ BDD + ".j5cho.mongodb.n
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à nodemonMongoDB échouée !'));
 
+app.use(express.json()); //on recupère le corps JSON des requête POST
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 
 
-  
+
+
 module.exports = app;
 
 
