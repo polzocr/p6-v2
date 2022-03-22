f56ca8825af6f1c510c93ee6182d6e10c88c2b75
Polzocr <micro.cpt.62@outlook.fr>
2022-03-22 12:05:58 +0100

    route post sauce effectué

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,5 +2,17 @@ const Sauce = require('../models/sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
+    });
+    sauce.save()
+    .then(() => res.status(201).json({message: 'Sauce créée avec succès !'}))
+    .catch(error => res.status(400).json({error}));
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -9,6 +9,7 @@ module.exports = (req, res, next) => {
         if(req.body.userId && req.body.userId !== userId){
             throw 'User Id non valable';
         } else {
+            console.log("C'est bon le 'auth' fonctionne !")
             next();
         }
     } catch(error) {
